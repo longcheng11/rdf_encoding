c1673aa048e4426fad3fa5e441d714c75f54a349
longcheng11 <paracheng@gmail.com>
2014-03-16 21:52:41 +0000

    Update gzRead.h

--- gzRead.h
+++ gzRead.h
@@ -1,3 +1,13 @@
+/*
+ *  This file is part of the RDF Encoding test code.
+ *
+ *  This file is licensed to You under the Eclipse Public License (EPL);
+ *  You may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *      http://www.opensource.org/licenses/eclipse-1.0.php
+ *
+ */
+
 #include <x10aux/config.h>
 #include <x10aux/alloc.h>
 #include <x10aux/class_cast.h>
